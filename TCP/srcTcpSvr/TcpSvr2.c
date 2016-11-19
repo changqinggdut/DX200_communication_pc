ff80c5fca54608510418ff88e3abb1d096af9c29
qiuwenbo <qiuwenbogdut@163.com>
2016-11-19 20:15:38 +0800

    返回字符aaa

---
+++ TCP/srcTcpSvr/TcpSvr2.c
@@ -0,0 +1,145 @@
+﻿/* TcpSvr2.c */
+/* Copyright 2009 YASKAWA ELECTRIC All Rights reserved. */
+
+#include "motoPlus.h"
+#include <string.h>
+
+// for API & FUNCTIONS
+void moto_plus0_task(void);//子函数的声明
+void ap_TCP_Sserver(ULONG portNo);//子函数的声明
+#define PORT        11000 //端口号，
+#define BUFF_MAX    1023
+
+void moto_plus0_task(void)
+{
+    puts("Activate moto_plus0_task!");
+
+    ap_TCP_Sserver(PORT);
+
+    mpSuspendSelf;//通讯程序停止，表示子任务的停止。
+}
+
+//char *strupr(char *string);
+char* location(char*string );
+
+void ap_TCP_Sserver(ULONG portNo)
+{
+    int     sockHandle;
+    struct  sockaddr_in  serverSockAddr;
+    int     rc;
+
+    printf("Simple TCP server\n");
+
+    sockHandle = mpSocket(AF_INET, SOCK_STREAM, 0);//创建socket,这里需要注意三个参数：第一个参数只有一种值，AF_INET表示使用ipv4通信簇， 确定通信类型：tcp或者tdp，该函数会输出一个新建的socket的描述符。
+    if (sockHandle < 0)//如果socket创建失败，进行返回
+        return;
+
+    memset(&serverSockAddr, 0, sizeof(serverSockAddr));//初始化，
+    serverSockAddr.sin_family = AF_INET; //该参数表示使用ipv4的协议。
+    serverSockAddr.sin_addr.s_addr = INADDR_ANY;//INADDR_ANY就是指定地址为0.0.0.0的地址，这个地址事实上表示不确定地址，或“所有地址”、“任意地址”。 一般来说，在各个系统中均定义成为0值。
+    serverSockAddr.sin_port = mpHtons(portNo);//将十进制的端口号转化成为网络形式的端口号。
+
+    rc = mpBind(sockHandle, (struct sockaddr *)&serverSockAddr, sizeof(serverSockAddr)); //绑定socket和端口号，对sockt绑定需要知道该socket的描述符，还有需要知道服务器端的端口地址。
+    if (rc < 0)
+        goto closeSockHandle;//关闭端口号，
+
+    rc = mpListen(sockHandle, SOMAXCONN);//监听端口号
+    if (rc < 0)
+        goto closeSockHandle;
+
+    while (1)
+    {
+        int     acceptHandle;
+        struct  sockaddr_in     clientSockAddr;
+        int     sizeofSockAddr;
+
+        memset(&clientSockAddr, 0, sizeof(clientSockAddr));
+        sizeofSockAddr = sizeof(clientSockAddr);
+
+        acceptHandle = mpAccept(sockHandle, (struct sockaddr *)&clientSockAddr, &sizeofSockAddr);//接收来自客户端的链接请求
+
+        if (acceptHandle < 0)
+            break;
+
+        while( 1 )
+        {
+            int     bytesRecv;
+            int     bytesSend;
+            char    buff[BUFF_MAX + 1];
+
+            memset(buff, 0, sizeof(buff));
+
+            bytesRecv = mpRecv(acceptHandle, buff, BUFF_MAX, 0);//从socket中提取字符
+            
+            puts(buff);
+
+            if (bytesRecv < 0)
+                break;
+
+            /* 受信データを大文字に変換して送り返す */
+           // strupr(buff);
+            location(buff);
+            bytesSend = mpSend(acceptHandle,buff, bytesRecv, 0);//向socket写入信息
+
+            if (bytesSend != bytesRecv)
+                break;
+
+            if (strncmp(buff, "EXIT", 4) == 0 || strncmp(buff, "exit", 4) == 0)
+                break;
+        }
+        mpClose(acceptHandle);//关闭socket
+    }
+closeSockHandle:
+    mpClose(sockHandle);//关闭socket
+
+    return;
+}
+
+/*char *strupr(char *string)//对字符实现转化，将小写字母转化成大写字母。
+{
+    int		i;
+    int     len;
+
+    len = strlen(string);
+	for (i=0; i < len; i++)
+	{
+	    if (isalpha((unsigned char)string[i]))
+	    {
+		    string[i] = toupper(string[i]);
+        }
+	}
+    return (string);
+}*/
+char* location(char*string )
+{
+   /*
+	int a=0;
+	const char * split = ",";
+	char * p;
+	p = strtok (string,split);
+	int i=0;
+	while(p!=NULL) 
+	{
+	  string[i]= atoi(p);
+	  i++;
+	  p = strtok(NULL,split);
+	}
+	//getchar();
+	
+	*/
+	int		i;
+    int     len;
+
+    len = strlen(string);
+	for (i=0; i < len; i++)
+	{
+	   
+	    
+		    string[i] ='a';
+        
+	}
+  
+	return (string);
+}
+
+
---
+++ 通信/client-socket_communication/socket_communication/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace SocketClient
+{
+    class Program
+    {
+        private static byte[] result = new byte[1024];
+        static void Main(string[] args)
+        {
+            //设定服务器IP地址
+            IPAddress ip = IPAddress.Parse("192.168.255.1");
+         
+
+            int port = 11000;
+           // string host = "127.0.0.1";
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //clientSocket.DualMode = true;
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, port)); //配置服务器IP与端口
+                Console.WriteLine("连接服务器成功");
+            }
+            catch
+            {
+                Console.WriteLine("连接服务器失败，请按回车键退出！");
+                return;
+            }
+            //建立并开启接收线程
+            Thread th = new Thread(recMsg);
+            th.IsBackground = true;
+            th.Start(clientSocket);
+
+            //通过clientSocket接收数据
+            //int receiveLength = clientSocket.Receive(result);
+            //Console.WriteLine("接收服务器消息：{0}", Encoding.ASCII.GetString(result, 0, receiveLength));
+            //通过 clientSocket 发送数据
+           //for (int i = 0; i < 10; i++)
+          //  {
+                try
+                {
+                        //Console.ReadLine();
+                        //Thread.Sleep(1000);    //等待1秒钟
+                        //string sendMessage = "fg";
+                       int [] myee;
+                        myee = new int [3] { 1,0,0 };
+                        string[] b;
+                        b = new string[3];
+                    
+                        for (int j = 0; j < 3; j++)
+                            {
+                             b[j] = myee[j].ToString();
+                            }
+                   
+                        string s = b[0]+","+b[1]+","+b[2];
+                        clientSocket.Send(Encoding.ASCII.GetBytes(s));    //Encoding.ASCII.GetBytes(sendMessage)   //a,a.Length,SocketFlags.None
+                        Console.WriteLine("向服务器发送消息：" + s);
+                }
+                catch
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    //break;
+                }
+
+           // }
+            Console.WriteLine("发送完毕，按回车键退出");
+            Console.ReadLine();
+        }
+
+        static void recMsg(object o)
+        {
+            Socket clientSocket = (Socket) o;
+            while (true)
+            {
+                try
+                {
+                    byte[] result = new byte[1024];
+                    int receiveLength = clientSocket.Receive(result);
+                    Console.WriteLine("接收服务器消息：{0}", Encoding.ASCII.GetString(result));
+
+
+                   // Encoding.ASCII.GetString(result,0,result.Length);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
+
