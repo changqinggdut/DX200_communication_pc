3c7de70d8fa6b8045a5e9ee9cc686be714d0cf8e
changqing <1667073018@qq.com>
2016-12-27 14:51:21 +0800

    12.27 改变平移矩阵1

--- 客户端/bo_communication_with_dx200/bo_communication_with_dx200/CoordinationTransformation.cs
+++ 客户端/bo_communication_with_dx200/bo_communication_with_dx200/CoordinationTransformation.cs
@@ -19,7 +19,7 @@ namespace bo_communication_with_dx200
     {
         public int[] sub(double x1, double y1)
         {
-            double a = -1.3184;double b = 3.1416;double c=-3.0828;//需要增加注释
+            double a = 3.1416;double b = 0;double c=2.8392;//需要增加注释
             int[] result = new int[2];
             double x0 = 0; double y0 = 0; double z0 = -228878;
 
@@ -28,9 +28,10 @@ namespace bo_communication_with_dx200
             V.Data[0, 1] = y0;
             V.Data[0, 2] = z0;
             Matrix<double> R = new Matrix<double>(1, 3);
-            R.Data[0, 0] = 77.3854 * x1 - 151680;
-            R.Data[0, 1] = 77.3854 * y1 - 57575;
-            R.Data[0, 2] = 0;
+            R.Data[0, 0] = 0.0699 * x1 - 37.8648;
+            R.Data[0, 1] = 0.0699 * y1 -43.0345;
+            R.Data[0, 2] = 0; 
+
             Matrix<double> S = new Matrix<double>(3, 3);
             S.Data[0, 0] = Math.Cos(a);
             S.Data[0, 1] = Math.Sin(a);
@@ -41,42 +42,53 @@ namespace bo_communication_with_dx200
             S.Data[2, 0] = 0;
             S.Data[2, 1] = 0;
             S.Data[2, 2] = 1;
-
+            Matrix<double> W = new Matrix<double>(3, 3);
+            W.Data[0, 0] = Math.Cos(b);
+            W.Data[0, 1] = Math.Sin(b);
+            W.Data[0, 2] = 0;
+            W.Data[1, 0] = -Math.Sin(b);
+            W.Data[1, 1] = Math.Cos(b);
+            W.Data[1, 2] = 0;
+            W.Data[2, 0] = 0;
+            W.Data[2, 1] = 0;
+            W.Data[2, 2] = 1;
             Matrix<double> T = new Matrix<double>(3, 3);
             T.Data[0, 0] = 1;
             T.Data[0, 1] = 0;
             T.Data[0, 2] = 0;
             T.Data[1, 0] = 0;
-            T.Data[1, 1] = Math.Cos(b);
-            T.Data[1, 2] = Math.Sin(b);
+            T.Data[1, 1] = Math.Cos(c);
+            T.Data[1, 2] = Math.Sin(c);
             T.Data[2, 0] = 0;
-            T.Data[2, 1] = -Math.Sin(b);
-            T.Data[2, 2] = Math.Cos(b);
+            T.Data[2, 1] = -Math.Sin(c);
+            T.Data[2, 2] = Math.Cos(c);
 
-            Matrix<double> W = new Matrix<double>(3, 3);
-            W.Data[0, 0] = Math.Cos(c);
-            W.Data[0, 1] = Math.Sin(c);
-            W.Data[0, 2] = 0;
-            W.Data[1, 0] = -Math.Sin(c);
-            W.Data[1, 1] = Math.Cos(c);
-            W.Data[1, 2] = 0;
-            W.Data[2, 0] = 0;
-            W.Data[2, 1] = 0;
-            W.Data[2, 2] = 1;
-
+            Matrix<double> T1 = new Matrix<double>(3, 3);
+            T1.Data[0, 0] = Math.Cos(c);
+            T1.Data[0, 1] = Math.Sin(c);
+            T1.Data[0, 2] = 0;
+            T1.Data[1, 0] = -Math.Sin(c);
+            T1.Data[1, 1] = Math.Cos(c);
+            T1.Data[1, 2] = 0;
+            T1.Data[2, 0] = 0;
+            T1.Data[2, 1] = 0;
+            T1.Data[2, 2] = 1;    
+          
             Matrix<double> U = new Matrix<double>(1, 3);
-            U.Data[0, 0] = 898220;
-            U.Data[0, 1] = -333120;
+            U.Data[0, 0] = 945.3303;
+            U.Data[0, 1] = -24.9752;
             U.Data[0, 2] = -228878;
 
 
             Matrix<double> S1 = new Matrix<double>(1, 3);
             Matrix<double> S2 = new Matrix<double>(1, 3);
-              Matrix<double> S3 = new Matrix<double>(1, 3);
+            Matrix<double> S3 = new Matrix<double>(1, 3);
+            Matrix<double> S4 = new Matrix<double>(1, 3);
             S1 = R.Mul(S);
-            S2 = S1.Mul(T);
-            S3=S2.Mul(W);
-            V = S3+ U;
+            S2 = S1.Mul(W);
+            S3=S2.Mul(T);
+            S4 = S3.Mul(T1);
+            V = S4+ U;
           
 
             result[0] =(int) (V.Data[0, 0]);
